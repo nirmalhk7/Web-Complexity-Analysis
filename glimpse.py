ab4086c8ae29caf891472696e8c6118f6eeba598
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-11-06 00:26:25 +0530

    Commit
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- glimpse.py
+++ glimpse.py
@@ -50,22 +50,3 @@ for t in webcategory.keys():
     print("{}: {}".format(t,webcategory[t]))
 
 print("")
-
-# Analyse number of object requests made
-
-# Analyse MIME types used.
-
-# Total Bytes downloaded
-
-# Number of distinct servers
-# '{uri.netloc}'.format(uri=urlparse(x))
-
-# Number of Non-Origin Services
-
-# Contribution of Non-Origin Services (in number of objects, bytes and contribution to the load time)
-
-# Content breakdown of Non-Origin Services
-
-# Origin vs Non-Origin Domains
-
-# Classifying Non-Origin domains
